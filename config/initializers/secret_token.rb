9ed9d2bc926006e224b34a5ab734f2cad20fc47c
Travis Vachon <travis@copious.com>
2011-06-16 15:10:26 -0700

    Project to demonstrate rails filter weirdness.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnlessFilterBug::Application.config.secret_token = 'a6c2a2d9762067a98c6a108385af4283658dc15e3ab625bc543d304d0aa4b6e214d1b1e84b01c1453c412d9499161f9bf995c8b372f188a758f1969f7e109acb'
